5c4040f1e513a2a40fe3ae873c075f9dedd12b75
reiskeks <felixbaral@yahoo.de>
2017-04-21 21:06:27 +0200

    added rows

--- src/defaultwidgets/object/object.widget.ts
+++ src/defaultwidgets/object/object.widget.ts
@@ -4,9 +4,9 @@ import { ObjectLayoutWidget } from '../../widget';
 
 @Component({
   selector: 'sf-form-object',
-  template: `<fieldset *ngFor="let fieldset of formProperty.schema.fieldsets">
+  template: `<fieldset class="row" *ngFor="let fieldset of formProperty.schema.fieldsets">
 	<legend *ngIf="fieldset.title">{{fieldset.title}}</legend>
-	<div *ngFor="let fieldId of fieldset.fields">
+	<div class="col-6" *ngFor="let fieldId of fieldset.fields">
 		<sf-form-element [formProperty]="formProperty.getProperty(fieldId)"></sf-form-element>
 	</div>
 </fieldset>`
